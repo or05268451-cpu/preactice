ab2e98cd39437e70fc01cc99ed753ffcbb7243dd
or05268451-cpu <or05268451@priestley.ac.uk>
2025-10-20 10:22:53 +0100

    Add files via upload

---
+++ programming algorithm 1 (fizzbuzz).py
@@ -0,0 +1,21 @@
+#ask the user to input a number from 1 tp 100 in turn
+for x in range(1, 101):
+    response = input("Enter a number")
+
+    if x % 3 == 0 and x % 5 == 0:
+        if (response != "fizz_buzz"):
+            print("Incorrect, the answer is fizz_buzz!")
+        
+    elif x % 3 == 0:
+        if (response != "fizz!"):
+            print("Incorrect, the answer is fizz!")
+        
+
+    elif x % 5 == 0:
+        if (response != "buzz!"):
+            print("Incorrect, the answer is buzz!")
+
+    else:
+        if (int(response) != x):
+            print("Incorrect, the answer is ", x)
+            

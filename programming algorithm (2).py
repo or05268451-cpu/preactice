55eee7183b03b48566be32019fcca511db517414
or05268451-cpu <or05268451@priestley.ac.uk>
2025-10-21 13:52:20 +0100

    Add files via upload

---
+++ programming algorithm (2).py
@@ -0,0 +1,24 @@
+#dividing integers by 2
+
+def user_input():
+    #ask user for a number
+    user_input = input("Please enter an integer: ")
+
+    #convert user_input to an integer
+    number = int(user_input)
+
+def number():
+    result == number % 2
+    if result > 0:
+        list = 0
+
+    if result < 0:
+        list = 1
+        
+
+
+    return result
+
+
+    
+

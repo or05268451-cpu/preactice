fde6df1a2ecdefad929f12af0e0ac332c3f6d24c
or05268451-cpu <or05268451@priestley.ac.uk>
2025-11-03 10:21:05 +0000

    Add files via upload

---
+++ coding linear search (1).py
@@ -0,0 +1,23 @@
+import random
+
+# create a list comprehension
+lista = [random.randrange(1, 100) for x in range (1, 1000)]
+found = False
+count = 0
+
+#ask a user to enter a search term
+search_term = int(input("Enter a search item"))
+
+# to create a loop to search through each data item in the list
+for x in range(len(lista)):
+    count += 1
+    #compares the search_item to data item in the list
+    if (search_term == lista[x]):
+        found = True
+        break
+#using an if statement, check to see if found is true or false
+if (found == True):
+    print(f'Found data item! in {count}')
+else:
+    print('Data item not found')
+        

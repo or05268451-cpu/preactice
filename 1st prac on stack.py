5fdca87f989755d1f7ab590801590fef75959779
or05268451-cpu <or05268451@priestley.ac.uk>
2025-12-15 10:27:23 +0000

    Add files via upload

---
+++ 1st prac on stack.py
@@ -0,0 +1,36 @@
+def stack():
+    
+    stack = []
+
+def PushStack():
+    
+    stack.append(5)
+    stack.append(10)
+    stack.append(15)
+    stack.append(20)
+
+def PopStack():
+    
+    stack.pop()
+    stack.pop()
+    stack.pop()
+
+def PeekStack():
+    pass
+    
+
+def menu():
+    stack = []
+    while True:
+        print("Push onto stack", (5))
+        print("Pop onto Stack")
+        #print("")
+        #print("")
+        #print("")
+
+    
+def main():
+    menu()
+
+if (__name__ == "__main__"):
+    main()
